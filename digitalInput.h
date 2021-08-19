ddbaa58b0923159891693674080f70e618f4b9cc
xtr0d3m0n <uchiha.pubudu@gmail.com>
2021-08-19 20:37:31 +0530

    fixed structures

--- digitalInput.h
+++ digitalInput.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <stdint.h> 
+#include <stdint.h>
+#include <stdbool.h> 
 
 #define INPUT_COUNT 10 /* define the number of inputs */
 
@@ -8,15 +9,15 @@
 
 typedef struct
 {
-    uint32_t lastPressed = 0;
-    bool pinState = true;
-    bool lastPinState = true;
-    bool lastGlitchPinState = true;
-    bool pressed = false;
-    bool shortPressed = false;
-    bool longPressed = false;
-    bool doublePressed = false;
-    bool level = true; /* Active high or low level of the input */
+    uint32_t lastPressed;
+    bool pinState;
+    bool lastPinState;
+    bool lastGlitchPinState;
+    bool pressed;
+    bool shortPressed;
+    bool longPressed;
+    bool doublePressed;
+    bool level; /* Active high or low level of the input */
 }digitalInput_t;
 
 void digitalInput(digitalInput_t *inputs);
